8768cc5a9497f7675ca9285cc644aa2c5146fd04
Navya Agrawal <navyaagrawal1823@gmail.com>
2025-11-22 19:11:16 +0000

    added basics js files

---
+++ datatypes-summary.js
@@ -0,0 +1,22 @@
+// primitive: String , number, boolean, null, undefined, symbol, bigInt
+const score=100;
+const scoreValue=100.1
+const isLoggedIn=true
+const temp=null 
+let email;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id==anotherId)
+const bigInt=1234567812
+// reference(non primitive)
+// array object function
+
+const ppl=["ishita","navya","navika"]
+let obj={
+    name:"navya",
+    age:18,
+}
+const myfunction=function(){
+    console.log("hello world");
+}
+console.log(typeof(temp))
