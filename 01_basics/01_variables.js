4df439c448576e912bcd6ee4b8c0964290b8eac2
Navya Agrawal <navyaagrawal1823@gmail.com>
2025-11-22 12:29:15 +0000

    Added basics JS files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=44221
+let accountEmail= "amisha@google.com"
+var accountPassword="23456"
+accountCity="Jaipur"
+let accountState;
+accountEmail="hdfc@gmail.com"
+accountPassword="212345"
+accountCity="Bengaluru"
+console.table([accountId, accountEmail,accountPassword,accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+console.log(3+4)
+console.log("Navya")
+let name="Navya"
+let age=18
+let isLoggedIn= true
+let state=null
+//number==2 to power 53
+// string=" "
+// boolean
+// null=stand alone value
+// undefined=not assigned anything
+// symbol=uniqueness
+console.log(typeof"Navya")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
