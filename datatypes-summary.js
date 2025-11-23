8152f64e011273aed88039761a3c8f75f5e76753
Navya Agrawal <navyaagrawal1823@gmail.com>
2025-11-23 18:42:25 +0000

    added basics js file

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -1,3 +1,5 @@
+
+
 // primitive: String , number, boolean, null, undefined, symbol, bigInt
 const score=100;
 const scoreValue=100.1
@@ -20,3 +22,19 @@ const myfunction=function(){
     console.log("hello world");
 }
 console.log(typeof(temp))
+// stack(primitive ) heap( non primitive)
+// stack: each variable gets in own copy , heap: variable share the same object reference
+let myName="navya"
+let anotherName= myName
+anotherName="agrawal"
+console.log(myName)
+console.log(anotherName)
+let user1={
+    email:"navya@goggle.com"
+   
+
+}
+let user2= user1
+user2.email="agrawal@goggle.com"
+console.log(user1.email)
+console.log(user2.email)
