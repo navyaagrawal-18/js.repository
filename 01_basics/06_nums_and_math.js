83230fac6bcc8f131cf8df86520896af0ce0deaa
Navya Agrawal <navyaagrawal1823@gmail.com>
2025-11-25 11:03:46 +0000

    added basics js files

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+const score=100
+console.log(score)
+const balance= new Number(100)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+const anotherScore=12.469
+console.log(anotherScore.toPrecision(3))
+console.log(anotherScore.toLocaleString('en-IN'))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.isFinite(100));
+console.log(Number.parseInt("20"))
+console.log(Number.parseFloat("3.14"))
+console.log(Number.NaN)
+// MATHS
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.max(4,3,6,8))
+console.log(Math.min(4,3,6,8))
+console.log(Math.random())
+console.log(Math.random()*10+1)
+console.log(Math.sqrt(16));
