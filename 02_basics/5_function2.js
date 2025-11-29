ede50dda047d5be983479e043851edaacc42e13f
Navya Agrawal <navyaagrawal1823@gmail.com>
2025-11-29 14:24:32 +0000

    added basics js files

---
+++ 02_basics/5_function2.js
@@ -0,0 +1,19 @@
+function calculatePrice(...nums1){
+    return nums1
+}
+console.log(calculatePrice(200,400,500))
+const user={
+    username:"navya",
+    price: 199
+
+}
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`)
+
+}
+handleObject(user)
+const arr=[200,400,600]
+function values(arr){
+    return arr[1]
+}
+console.log(values(arr))
