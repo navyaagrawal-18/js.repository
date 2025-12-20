f4769dd102a5c3e6af7be8f21b1f48c8d225c323
Navya Agrawal <navyaagrawal1823@gmail.com>
2025-12-20 05:48:48 +0000

    project1 solution code

--- 04_iterations/4_Filtermap.js
+++ 04_iterations/4_Filtermap.js
@@ -10,3 +10,24 @@ console.log(changeresult);
 const arr=[1,2,3,4,5];
 const sum= arr.reduce((total, num)=> total+num,0);
 console.log(sum);
+// chaining
+const mynums=[1,2,3,4,5];
+const total=mynums.map(num=> num+10)
+.map(num=> num*10)
+.filter(num=>num>=40);
+console.log(total);
+// from given example set the condn.
+const people=[{
+    name:'navya',gender:'female',city:'hathras'
+},
+{
+    name:'ishita',gender:'female',city:'aligarh'
+},
+{
+    name:'madhav',gender:'male',city:'hathras'
+},
+{
+    name:'tanmay',gender:'male',city:'mathura'
+}]
+const ans=people.filter(ppl=>ppl.gender==='female'&& ppl.city==='hathras')
+console.log(ans);
